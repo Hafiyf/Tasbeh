9038c2a27d9887a72254c1475626bf81b2d18af6
Jamshid <59602019+JOA4K@users.noreply.github.com>
2022-12-20 18:35:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const resetBtn = document.querySelector('.resetBtn')
+const title = document.querySelector('.count-title')
+const incBtn = document.querySelector('.incBtn')
+
+let count = 0;
+
+
+incBtn.addEventListener('click', () => {
+    count++
+    title.innerHTML = count;
+    console.log(count);
+})
+
+resetBtn.addEventListener('click', () =>{
+    count = 0;
+    title.innerHTML = count;
+    console.log(count);
+})
+
